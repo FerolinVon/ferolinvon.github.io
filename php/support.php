3979bb80ec00b39763eddc3f77b586ad80c96d98
FerolinVon <ludwig1215@gmail.com>
2019-10-29 12:59:54 +0800

    uploaded files

---
+++ php/support.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>VONFEROLIN</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="default.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="wrapper">
+	<div id="header">
+	  <h1>VON FEROLIN</h1>
+	  <p><span>BSIT 2-2</span></p>
+	</div>
+	<!-- end header -->
+	<div id="menu">
+		<h2>Main Menu</h2>
+		<ul>
+			<li class="active"><a href="index.html">Home</a></li>
+			<li><a href="product.html">Products</a></li>
+			<li><a href="services.html">Services</a></li>
+			<li><a href="forum.html">Forums</a></li>
+			<li><a href="support.html">Support</a></li>
+			<li><a href="about.html">About</a></li>
+			<li><a href="contact.html">Contact</a></li>
+		</ul>
+	</div>
+	<!-- end menu -->
+	<div id="page">
+		<div id="content">
+		  <h1 class="title"><span>SUPPORT </span></h1>
+<div class="boxed">
+  <h2 class="title">Since 2013 support dota 2 player!</h2>
+  <div class="content">
+    <p>Most Successful heroes<br />
+    </p>
+    <p>Crystail Maiden<br />
+      Necrophos<br />
+      DarkSeer<br />
+      Keeper of the Light <br />
+      Lion<br />
+      Vissage<br />
+      IO<br />
+      Chen<br />
+      Pudge<br />
+      Pugna<br />
+      Shadow Shaman<br />
+      Dazle<br />
+      Warlock<br />
+      Abadon<br />
+      Apparition<br />
+      Witch Doctor<br />
+      Enchantres<br />
+      Winter Wyvern<br />
+      Pheonex<br />
+      Rubick<br />
+      Omni Knight<br />
+      Enigma<br />
+      Beast Master </p>
+<blockquote>
+  <p>“ Oo Bobo ako sa Laro pero eto tandaan mo. Ako lang naman ang Bobo, ung Taong Bobo-o sa puso mo .. Ayieeeeeee.....”</p>
+</blockquote>
+				</div>
+			</div>
+		</div>
+		<!-- end content -->
+		<div id="sidebar">
+			<ul>
+				<li id="search">
+					<form id="searchform" method="get" action="">
+						<fieldset>
+						<legend>Search</legend>
+						<input id="searchfield" type="text" name="q" value="" />
+						<input id="searchsubmit" type="submit" value="Search" />
+						</fieldset>
+					</form>
+				</li>
+				<li id="submenu">
+					<h2>Other Links</h2>
+					<ul>
+					  <li><a href="https://www.facebook.com/ludwig.ferolin">Facebook</a></li>
+					  <li><a href="https://www.instagram.com/?hl=en">Instragram</a></li>
+					  <li><a href="https://twitter.com/?lang=en">Twitter</a></li>
+					  <li><a href="https://www.snapchat.com/">SnapChat</a></li>
+					  <li><a href="https://www.tiktok.com/">Tiktok</a></li>
+					  <li><a href="https://www.pinterest.ph/">Pinterest</a></li>
+					  
+					</ul>
+				</li>
+				<li id="news">
+					<h2>News &amp; Updates</h2>
+					<ul>
+						<li>
+						  <h3>January</h3>
+						  <p><a href="#">Sinulog sa CEBU or Kabankalan</a></p>
+						</li>
+						<li>
+						  <h3>Febuary</h3>
+						  <p><a href="#">Valentines Day</a></p>
+						</li>
+						<li>
+						  <h3>March</h3>
+						  <p><a href="#">Graduation sa mga ma graduate</a></p>
+						</li>
+						<li>
+						  <h3>April</h3>
+						  <p><a href="#">Bakasyon na!</a></p>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<!-- end sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<!-- end page -->
+	<div id="footer">
+		<p id="legal">Copyright &copy; The Island. All Rights Reserved. Designed by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+		<p id="links"><a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a></p>
+	</div>
+	<!-- end footer -->
+</div>
+<!-- end wrapper -->
+</body>
+</html>
